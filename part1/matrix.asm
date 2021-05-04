7e09829905b14b7616f6d9cb8bc71e83459386bf
Dor <dor.shacham@gmail.com>
2021-05-04 20:07:50 +0300

    git_elemnt was added

--- part1/matrix.asm
+++ part1/matrix.asm
@@ -4,6 +4,19 @@
 .section .text
 get_elemnt_from_matrix:
 	#TODO: STUDENTS NEED TO FILL
+        pushq %rbp #prolog -it is not realy needed here...
+        movq %rsp, %rbp
+        
+        xorq %rax, %rax
+        movq %rsi, %eax
+        mulq %edx # finding n*row
+        shlq $32, %rdx #putting the resulut in rdx
+        addq %rax, %rdx
+        
+        leaq (%rdi, %rdx, 4), %rdi #finding the right row
+        movq (%rdi, %ecx, 4), %eax 
+        
+        leave
 	ret
 
 multiplyMatrices:
