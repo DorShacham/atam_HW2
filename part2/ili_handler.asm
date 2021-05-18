6dd25bf4799b2c69d0205474d8283ac79151b0cb
Dor Shacham <dor.shacham@gmail.com>
2021-05-18 17:30:19 +0000

    ili_handler was writen

--- part2/ili_handler.asm
+++ part2/ili_handler.asm
@@ -4,5 +4,31 @@
 .align 4, 0x90
 my_ili_handler:
   ####### Some smart student's code here #######
-   
+      pushq %rsi #backup rsi
+      movq 8(%rsp), %rsi # rsi <- rip
+      cmpb $0x0F, (%rsi) #cheking if the opcode is one or two byte
+      je Two_byte
+      pushq $1
+      movb (%rsi), %dil
+continue:  
+       call what_to_do
+       cmpl $0, %eax
+       jne my_handler
+       popq %rsi 
+       popq %rsi
+       jmp *old_ili_handler
+
+my_handler:
+       popq %rdi # need to know if to move 1 or 2 bytes
+       addq %rdi, (%rsp)
+       popq %rsi
+       movl %eax, %edi
+
   iretq
+
+
+ Two_byte:
+       pushq $2
+       movb 1(%rsi), %dil
+       jmp continue
+       
