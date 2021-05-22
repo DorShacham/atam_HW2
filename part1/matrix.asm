08c3a54b03c824885d0c597b8fde3416834a0a8d
Itai Spigelman <itai10020@gmail.com>
2021-05-22 20:53:43 +0300

    done part b (without testing)

--- part1/matrix.asm
+++ part1/matrix.asm
@@ -1,4 +1,9 @@
 .global get_elemnt_from_matrix, multiplyMatrices
+# %rdi: int* matrix
+# %rsi: int n
+# %rdx: int row
+# %rcx: int col
+# %r8:  int value
 .extern set_elemnt_in_matrix
 
 # note to self, %rax,%rdi,%rsi,%rdx,%rcx,%r8-11 are caller-save 
@@ -19,6 +24,120 @@ get_elemnt_from_matrix:
 	popq %rbp
 	ret
 	
+# %rdi: int* first
+# %rsi: int* second
+# %rdx: int* result
+# %rcx: int m
+# %r8:  int n
+# %r9:  int r
+# (%rbp+0x10): unsigned int p
 multiplyMatrices:
-	#TODO: STUDENTS NEED TO FILL
+	pushq %rbp
+	movq %rsp, %rbp
+	pushq %r12
+	pushq %rbx
+	pushq %r13
+
+	pushq %rdi
+	pushq %rsi
+	pushq %rdx
+	pushq %rcx
+	pushq %r8
+
+	xor %r10, %r10 # i = 0
+loop_i:
+	cmp %r10, %rcx 
+	jle end
+	xor %r11, %r11 # j = 0
+loop_j:
+	cmp %r11, %r9
+	jg cont_j
+	inc %r10
+	jmp loop_i
+cont_j:
+	movq $0, %r13
+	movq %rdx, %rdi # result
+	movq %r9, %rsi # column size = r
+	movq %r10, %rdx # row = i
+	movq %r11, %rcx # column = j
+	movq %r13, %r8 # value = 0
+	call set_elemnt_in_matrix
+
+	movq -0x40(%rbp), %r8
+	movq -0x38(%rbp), %rcx
+	movq -0x30(%rbp), %rdx
+	movq -0x28(%rbp), %rsi
+	movq -0x20(%rbp), %rdi
+
+	xor %r12, %r12 # k = 0
+loop_k:
+	cmp %r12, %r8
+	jg cont_k
+	inc %r11
+	jmp loop_j
+cont_k:
+	# get first[i][k]
+	# %rdi already on matrix 1
+	movq %r8, %rsi # n = n
+	movq %r10, %rdx # row = i  
+	movq %r12, %rcx # col = k
+	call get_elemnt_from_matrix
+	movq %rax, %rbx 
+
+	movq -0x38(%rbp), %rcx
+	movq -0x30(%rbp), %rdx
+	movq -0x28(%rbp), %rsi
+	movq -0x20(%rbp), %rdi
+
+	# get second[k][j]
+	movq %rsi, %rdi
+	movq %r9, %rsi # n = r
+	movq %r12, %rdx # row = k  
+	movq %r11, %rcx # col = j
+	call get_elemnt_from_matrix
+
+	movq -0x38(%rbp), %rcx
+	movq -0x30(%rbp), %rdx
+	movq -0x28(%rbp), %rsi
+	movq -0x20(%rbp), %rdi
+
+	# %rbx = first[i][k]*second[k][j]
+	imul %rax, %rbx
+	# %r13 = result[i][j] + first[i][k]*second[k][j]
+	addq %rbx, %r13
+
+	# %r13 = (result[i][j] + first[i][k]*second[k][j]) % p
+	pushq %rdx
+	xor %rdx, %rdx
+	movq %r13, %rax
+	idivl 0x10(%rbp)
+	movq %rdx, %r13 
+	popq %rdx
+
+	movq %rdx, %rdi # result
+	movq %r9, %rsi # column size = r
+	movq %r10, %rdx # row = i
+	movq %r11, %rcx # column = j
+	movq %r13, %r8 # value = (result[i][j] + first[i][k]*second[k][j]) % p
+	call set_elemnt_in_matrix
+
+	movq -0x40(%rbp), %r8
+	movq -0x38(%rbp), %rcx
+	movq -0x30(%rbp), %rdx
+	movq -0x28(%rbp), %rsi
+	movq -0x20(%rbp), %rdi
+
+	inc %r12
+	jmp loop_k
+end:
+	popq %r8
+	popq %rcx
+	popq %rdx
+	popq %rsi
+	popq %rdi
+	
+	pop %r13
+	pop %rbx
+	pop %r12
+	pop %rbp
 	ret
--- part1/playground.c
+++ part1/playground.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int get_elemnt_from_matrix(int* matrix, int n, int row, int col);
+void multiplyMatrices(int* first, int* second, int* result, int m, int n, int r, unsigned int p);
 
-int main() {
+void set_elemnt_in_matrix(int* matrix, int num_of_columns, int row, int col, int value) {
+   *((matrix + row*num_of_columns) + col) = value;
+}
+
+void tryGetElement() {
     int matrix[] = {
         1,2,3,
         4,5,6,
@@ -12,5 +18,25 @@ int main() {
     int element = get_elemnt_from_matrix((int*)matrix, 3, 2, 1);
 
     printf("%d\n", element);
+}
+
+void tryMultiplyMatrices() {
+    int matrix1[] = {
+        2,4,5,
+        5,2,7
+    };
+    int matrix2[] = {
+        4,1,
+        1,3,
+        6,0
+    };
+    int *res = (int*)malloc(sizeof(int)*2*2);
+    multiplyMatrices((int*)matrix1, (int*)matrix2, res, 2, 3, 2, 5);
+
+    printf("%d %d\n%d %d\n", res[0], res[1], res[2], res[3]);
+}
+
+int main() {
+    tryMultiplyMatrices();
 }
 
